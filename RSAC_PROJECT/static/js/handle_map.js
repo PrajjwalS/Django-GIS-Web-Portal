127060b9c91680db95fca101de370916fda3693d
Prajjwal <prajjwals12th@gmail.com>
2020-09-22 08:22:12 +0530

    final website

--- RSAC_PROJECT/static/js/handle_map.js
+++ RSAC_PROJECT/static/js/handle_map.js
@@ -14,25 +14,10 @@ for(i in Object.keys(base_layers))
 }
 var default_base_layer_name = "Google Street Map";
 var current_base_layer = base_layers[default_base_layer_name];
-console.log(current_base_layer);
 base_select_tag.selectedIndex = Object.keys(base_layers).indexOf(default_base_layer_name);
 current_base_layer.addTo(map);
 
 /////////////////////////////////////////////////////////////////
-
-
-
-//////// poppulating overlay layer set selector with options /////////
-// var overlay_select_tag = document.getElementById("select_overlay_layer_set");
-// for(i in Object.keys(overlay_layer_sets))
-// {
-//    var opt = document.createElement("option");
-//    opt.value= Object.keys(overlay_layer_sets)[i];
-//    opt.innerHTML = Object.keys(overlay_layer_sets)[i]; 
-//    overlay_select_tag.appendChild(opt);
-// }
-
-///////////////////////////////////////////////////////////////////////
 
 
 ///// managing control for base layers ////
@@ -48,42 +33,10 @@ function manage_selected_base_layer(selectedObject)
 
 
 ///// managing control for overlay layers ////
-// function manage_selected_layers(selectedObject)
-// {
-//   // this functions gets called when new overlay layer is selected from UI
-//   layer_control = L.control.layers(null,overlay_layer_sets);
-  
-// }
 layer_control = L.control.layers(null,overlay_layer_sets);
 layer_control.addTo(map);
 
-///////////////////////////////////////////////
 
-///// managing control for overlay layer sets ////
-// current_layer_control = L.control.layers();
-// function manage_selected_layer_set_controller(selectObject) 
-// {
-//   // this function gets called when new layer set is selected from UI
-//    map.eachLayer(function (layer) 
-//    {
-//      if(layer.options.pane=="overlay_pane" || layer.options.pane=="overlay_marker_pane")
-//        map.removeLayer(layer);
-//    });
-//    current_layer_control.remove();
-
-//    var val = selectObject.value;
-   
-//    if(val == "None")
-//     return;
-//    else 
-//      current_layer_control = L.control.layers(null, overlay_layer_sets[val],overlay_layer_sets[val]);
-
-//    // adding selected layer set's controller to map
-//    current_layer_control.addTo(map);
-//    // applying selected layer set's first layer to map
-//    overlay_layer_sets[val][Object.keys(overlay_layer_sets[val])[0]].addTo(map);
-// }
-//////////////////////////////////////////////////
 
 //////// managing buffer radius input for point layers /////
 current_point_buffer_layer = L.geoJSON();
